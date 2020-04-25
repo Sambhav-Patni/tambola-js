61f1510a0ba1162d38ccdaf14ba3af00e263da85
Source Of Arising Mentality <sambhav181@gmail.com>
2020-04-26 00:17:01 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,22 +5,15 @@ var express  = require('express')
   , cors 	   = require('cors')
   , host     = 'localhost'
   , server   = http.createServer(app).listen(process.env.PORT || 8080, host)
-  , io       = require('socket.io')(server, {
-    handlePreflightRequest: (req, res) => {
-        const headers = {
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-            "Access-Control-Allow-Credentials": true
-        };
-        res.writeHead(200, headers);
-        res.end();
-    }
-})//.listen(server)
-  ;
+  , io       = require('socket.io')(server, { origins: '*:*'})
+;
 
-app.use(express.static(__dirname + "/../client"));
-app.use(cors());
-app.options('*', cors());
+//app.use(express.static(__dirname + "/../client"));
+//app.use(cors());
+//app.options('*', cors());
+app.use(function (req, res){  
+console.log(req.url);
+res.sendFile(req.url, { root: __dirname+ "/client" })})  
 
 //random number
 var ticketArray = []
